beba857aeda7a83264508e0a11e9a6b65646f258
Anthony Panettiere <apanettiere@fastmail.com>
2022-12-01 17:00:52 -0600

    Implement Elf

---
+++ Main/Elf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Elf : IElf
+    {
+        /// <inheritdoc/>
+        public List<int> Calories {get; internal set;} = new List<int>();
+
+        /// <inheritdoc/>
+        public void AddCalorie(int calorieAmount)
+        {
+            Calories.Add(calorieAmount);
+        }
+
+        /// <inheritdoc/>
+        public int CalculateCalorieTotal()
+        {
+            return Calories.Sum();
+        }
+    }
+}
---
+++ Main/IElf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public interface IElf
+    {
+        /// <summary>
+        /// Adds the provided calorie amount to the calorie list.
+        /// </summary>
+        /// <param name="calorieAmount">Amount to add to the calorie list.</param>
+        void AddCalorie(int calorieAmount);
+
+        /// <summary>
+        /// The list of calories this elf has.
+        /// </summary>
+        List<int> Calories { get; }
+
+        /// <summary>
+        /// Calculates the total number of calories this elf has.
+        /// </summary>
+        /// <returns>The calorie total.</returns>
+        int CalculateCalorieTotal();
+    }
+}
