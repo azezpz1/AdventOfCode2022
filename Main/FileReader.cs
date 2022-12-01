ee8b8376b7ab9db251252bea01d8d9384fff89d3
Anthony Panettiere <apanettiere@fastmail.com>
2022-12-01 17:11:39 -0600

    Create FileReader
    Should make testing easier

---
+++ Main/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class FileReader : IFileReader
+    {
+        public FileReader(string path)
+        {
+            Path = path;
+        }
+
+        string Path { get; set; }
+        
+        IEnumerable<string> ReadLines() => File.ReadLines(Path);
+    }
+}
