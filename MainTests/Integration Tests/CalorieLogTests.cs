b601f8841d1e2a948c9d5f3d74b5eacde24c33dc
Anthony Panettiere <apanettiere@fastmail.com>
2022-12-01 19:56:23 -0600

    Add tests for part 1

---
+++ MainTests/Integration Tests/CalorieLogTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Main;
+using Xunit;
+
+namespace MainTests.IntegrationTests
+{
+    public class CalorieLogTests
+    {
+        [Fact]
+        public void ExampleTest_CalculateMostCalorieElfTotal_Calculates24000()
+        {
+            var fileReader = new FileReader(
+                @"Integration Tests/TestFiles/CalorieLog/ExampleTest.txt");
+            var calorieLog = new CalorieLog(fileReader);
+            var expected = 24000;
+            calorieLog.ReadFile();
+
+            var result = calorieLog.CalculateMostCalorieElfTotal();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void FirstHalfTest_CalculateMostCalorieElfTotal_Calculates24000()
+        {
+            var fileReader = new FileReader(
+                @"Integration Tests/TestFiles/CalorieLog/FirstHalfTest.txt");
+            var calorieLog = new CalorieLog(fileReader);
+            var expected = 67450;
+            calorieLog.ReadFile();
+
+            var result = calorieLog.CalculateMostCalorieElfTotal();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
